9522e27aaa2a6d8ce8be61a8d8a5dc4350be0c66
mettcm <mettcm@msoe.edu>
2024-04-29 12:55:01 -0500

    there was a problem with the delete method -- if deleted key was in top most level, it wouldn't delete. This should now be fixed

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,6 @@
 public class Driver {
     // Driver to test above code
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // create SkipList object with MAXLVL and P
         SkipList lst = new SkipList(3, 0.5f);
 
@@ -16,13 +15,97 @@ public class Driver {
         lst.insertElement(26);
         lst.insertElement(21);
         lst.insertElement(25);
+
+        System.out.println("P = 0.5");
+
         lst.displayList();
 
         lst.deleteElement(9);
         lst.deleteElement(12);
         lst.displayList();
 
-        lst.search(9);
+//        lst.search(9);
+
+        pLevelTest0_25();
+    }
+
+    //different P levels
+    public static void pLevelTest0_25(){
+        // create SkipList object with MAXLVL and P
+        SkipList lst = new SkipList(3, 0.25f);
+
+        lst.insertElement(3);
+        lst.insertElement(6);
+        lst.insertElement(7);
+        lst.insertElement(9);
+        lst.insertElement(30);
+        lst.insertElement(12);
+        lst.insertElement(19);
+        lst.insertElement(17);
+        lst.insertElement(26);
+        lst.insertElement(21);
+        lst.insertElement(25);
+
+        System.out.println("P = 0.25");
+
+        lst.displayList();
+
+        lst.deleteElement(9);
+        lst.deleteElement(12);
+        lst.displayList();
+
+    }
+
+    public static void pLevelTest0_75(){
+        // create SkipList object with MAXLVL and P
+        SkipList lst = new SkipList(3, 0.75f);
+
+        lst.insertElement(3);
+        lst.insertElement(6);
+        lst.insertElement(7);
+        lst.insertElement(9);
+        lst.insertElement(30);
+        lst.insertElement(12);
+        lst.insertElement(19);
+        lst.insertElement(17);
+        lst.insertElement(26);
+        lst.insertElement(21);
+        lst.insertElement(25);
+
+        System.out.println("P = 0.75");
+
+        lst.displayList();
+
+        lst.deleteElement(9);
+        lst.deleteElement(12);
+        lst.displayList();
+
+
+    }
+
+    public static void pLevelTest0_90(){
+        // create SkipList object with MAXLVL and P
+        SkipList lst = new SkipList(3, 0.90f);
+
+        lst.insertElement(3);
+        lst.insertElement(6);
+        lst.insertElement(7);
+        lst.insertElement(9);
+        lst.insertElement(30);
+        lst.insertElement(12);
+        lst.insertElement(19);
+        lst.insertElement(17);
+        lst.insertElement(26);
+        lst.insertElement(21);
+        lst.insertElement(25);
+
+        System.out.println("P = 0.90");
+
+        lst.displayList();
+
+        lst.deleteElement(9);
+        lst.deleteElement(12);
+        lst.displayList();
 
     }
 }
--- src/SkipList.java
+++ src/SkipList.java
@@ -13,8 +13,7 @@ public class SkipList {
     // pointer to header node
     Node header;
 
-    SkipList(int MAXLVL, float P)
-    {
+    SkipList(int MAXLVL, float P) {
         this.MAXLVL = MAXLVL;
         this.P = P;
         level = 0;
@@ -23,8 +22,7 @@ public class SkipList {
         header = new Node(-1, MAXLVL);
     }
 
-    int randomLevel()
-    {
+    int randomLevel() {
         float r = (float)Math.random();
         int lvl = 0;
         while (r < P && lvl < MAXLVL) {
@@ -34,52 +32,41 @@ public class SkipList {
         return lvl;
     }
 
-    Node createNode(int key, int level)
-    {
+    Node createNode(int key, int level) {
         Node n = new Node(key, level);
         return n;
     }
 
-    // Insert given key in skip list
-
-    void insertElement(int key)
-    {
+    /**
+     * Insert given key in skip list
+     */
+    void insertElement(int key) {
         Node current = header;
 
         // create update array and initialize it
-        Node update[] = new Node[MAXLVL + 1];
+        Node[] update = new Node[MAXLVL + 1];
 
-            /* start from highest level of skip list
-                    move the current pointer forward while
-            key is greater than key of node next to
-            current Otherwise inserted current in update
-            and move one level down and continue search
-            */
+        /* start from the highest level of skip list move the current pointer forward while
+        key is greater than key of node next to current Otherwise inserted current in update
+        and move one level down and continue search */
         for (int i = level; i >= 0; i--) {
             while (current.forward[i] != null && current.forward[i].key < key)
                 current = current.forward[i];
             update[i] = current;
         }
 
-            /* reached level 0 and forward pointer to
-            right, which is desired position to
-            insert key.
-            */
+        /* reached level 0 and forward pointer to right, which is desired position to insert key. */
         current = current.forward[0];
 
-            /* if current is NULL that means we have reached
-            to end of the level or current's key is not
-            equal to key to insert that means we have to
-            insert node between update[0] and current node
+        /* if current is NULL that means we have reached to end of the level or current's key is not
+        equal to key to insert that means we have to insert node between update[0] and current node
         */
         if (current == null || current.key != key) {
             // Generate a random level for node
             int rlevel = randomLevel();
 
-            // If random level is greater than list's
-            // current level (node with highest level
-            // inserted in list so far), initialize
-            // update value with pointer to header for
+            // If random level is greater than list's current level (node with highest level
+            // inserted in list so far), initialize update value with pointer to header for
             // further use
             if (rlevel > level) {
                 for (int i = level + 1; i < rlevel + 1;
@@ -90,8 +77,7 @@ public class SkipList {
                 level = rlevel;
             }
 
-            // create new node with random level
-            // generated
+            // create new node with random level generated
             Node n = createNode(key, rlevel);
 
             // insert node by rearranging pointers
@@ -99,15 +85,15 @@ public class SkipList {
                 n.forward[i] = update[i].forward[i];
                 update[i].forward[i] = n;
             }
-            System.out.println(
-                    "Successfully Inserted key " + key);
+
+            System.out.println("Successfully Inserted key " + key);
         }
     }
 
     // Display skip list level wise
-    void displayList()
-    {
+    void displayList() {
         System.out.println("\n*****Skip List*****");
+
         for (int i = 0; i <= level; i++) {
             Node node = header.forward[i];
             System.out.print("Level " + i + ": ");
@@ -115,77 +101,64 @@ public class SkipList {
                 System.out.print(node.key + " ");
                 node = node.forward[i];
             }
+
             System.out.println();
         }
     }
+
     void deleteElement(int key){
         Node current = header;
         // create update array and initialize it
         Node[] update = new Node[MAXLVL+1];
 
-            /* start from highest level of skip list
-                    move the current pointer forward while
-            key is greater than key of node next to
-            current Otherwise inserted current in update
-            and move one level down and continue search
-            */
+        /* start from highest level of skip list move the current pointer forward while
+        key is greater than key of node next to current Otherwise inserted current in update
+        and move one level down and continue search*/
         for (int i = level; i >= 0; i--) {
             while (current.forward[i] != null && current.forward[i].key < key)
                 current = current.forward[i];
             update[i] = current;
         }
 
-            /* reached level 0 and forward pointer to
-            right, which is desired position to
-            delete key.
-            */
+        /* reached level 0 and forward pointer to right, which is desired position to delete key. */
         current=current.forward[0];
 
         if(current!=null && current.key==key){
-
             // delete node by rearranging pointers
-            for (int i = 0; i < level; i++) {
+            for (int i = 0; i <= level; i++) {
                 if (update[i].forward[i] != current)
                     break;
                 update[i].forward[i] = current.forward[i];
             }
 
             //if it was the only node in that level and you deleted it, delete the level
-            while(level>0 && header.forward[level] ==null){
+            while(level>0 && header.forward[level] == null){
                 level --;
             }
+
             System.out.println("Successfully deleted key "+ key);
         }
-
     }
 
     void search(int key){
-
         Node current = header;
 
-
-
-            /* start from highest level of skip list
-                    move the current pointer forward while
-            key is greater than key of node next to
-            current    and move one level down and continue search
-            */
+            /* start from highest level of skip list move the current pointer forward while
+            key is greater than key of node next to current and move one level down and continue search */
         for (int i = level; i >=0 ; i--) {
             while (current.forward[i] != null && current.forward[i].key < key) {
                 current = current.forward[i];
             }
         }
+
         current = current.forward[0];
+
         // current has to be the key if it is present
         if(current !=null && current.key==key) {
             System.out.println("Key found");
 
-        }
-        else{
+        } else{
             System.out.println("Key not found");
         }
-
-
-
     }
 }
